4d26c4602685dc29146a2ab2a07165cf51c0cf3b
abhiramatluri42 <91764285+abhiramatluri42@users.noreply.github.com>
2023-09-02 14:12:31 -0500

    Week 1

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,13 +13,15 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj.PS4Controller.Button;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.Wheel;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -36,7 +38,7 @@ public class RobotContainer {
   private final DriveSubsystem m_robotDrive = new DriveSubsystem();
 
   // The driver's controller
-  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+  CommandXboxController m_driverController = new CommandXboxController(OIConstants.kDriverControllerPort);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -68,10 +70,11 @@ public class RobotContainer {
    * {@link JoystickButton}.
    */
   private void configureButtonBindings() {
-    new JoystickButton(m_driverController, Button.kR1.value)
+    m_driverController.rightBumper()
         .whileTrue(new RunCommand(
             () -> m_robotDrive.setX(),
             m_robotDrive));
+    m_driverController.a().onTrue(new InstantCommand(() -> Wheel.forward()));
   }
 
   /**
--- src/main/java/frc/robot/subsystems/Wheel.java
+++ src/main/java/frc/robot/subsystems/Wheel.java
@@ -5,11 +5,28 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Wheel extends SubsystemBase {
+  public static CANSparkMax Wheel = new CANSparkMax(0, MotorType.kBrushless);
   /** Creates a new Wheel. */
-  public Wheel() {}
+  public Wheel() {
+      
+  }
 
+  public static void forward() {
+    Wheel.setVoltage(1);
+  }
+
+  public void backward() {
+    Wheel.setVoltage(-1);
+  }
+
+  public void stop() {
+    Wheel.setVoltage(0);
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
